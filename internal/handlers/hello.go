44f55d812018c8ef31b9ed3cf27b754ffd41a5d8
Vlad <viamont@yandex.ru>
2026-05-08 15:19:24 +0300

    add if/else hello.go

--- internal/handlers/hello.go
+++ internal/handlers/hello.go
@@ -11,11 +11,22 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	if name == "" {
 		name = "имя не введено"
 	}
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(map[string]string{
-		"message": "Hello " + name,
-	})
-	if err != nil {
-		fmt.Printf("Ошибка при кодировании JSON: %v", err)
+	if name == "Vlad" {
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(map[string]string{
+			"message": "Admin: " + name,
+		})
+		if err != nil {
+			fmt.Printf("Ошибка при кодировании JSON: %v", err)
+		}
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		err := json.NewEncoder(w).Encode(map[string]string{
+			"message": "Hello " + name,
+		})
+		if err != nil {
+			fmt.Printf("Ошибка при кодировании JSON: %v", err)
+		}
 	}
+
 }
