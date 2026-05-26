f0c9fdbfd9a4fadb00f180fb4172e14de9a1a5b1
Vlad <viamont@yandex.ru>
2026-05-26 14:49:48 +0300

    -

--- internal/handlers/hello_test.go
+++ internal/handlers/hello_test.go
@@ -1,1 +1,20 @@
 package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHello(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /hello/{name}", HelloHandler)
+
+	req := httptest.NewRequest(http.MethodGet, "/hello/Name", nil)
+	rr := httptest.NewRecorder()
+
+	mux.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Ожидался статус 200 OK, получен %v", status)
+	}
+}
